4b2f82dde6bf97b79542335678e09b87023934ef
Diogo Queiroz <diogo.qds97@gmail.com>
2021-05-04 18:41:32 -0300

    first commit

---
+++ lib/list_length.ex
@@ -0,0 +1,18 @@
+defmodule ListLength do
+  @moduledoc """
+  Documentation for `ListLength`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ListLength.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
